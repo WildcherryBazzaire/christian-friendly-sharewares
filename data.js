917ff1a09ace665ccea527743652a6723582f882
Alex <eieghtbitdolphin@gmail.com>
2018-09-22 03:00:25 -1000

    in progress

---
+++ data.js
@@ -0,0 +1,26 @@
+/* data for beach locations */
+var beachLocations = {
+    whiteplains: [
+        {lat:21.300776, lng:-158.051877},
+        {lat: 21.303286, lng:-158.045182},
+        {lat:21.300968,lng:-158.044601},
+        {lat: 21.299284,lng:-158.051803}
+    ]
+}
+
+/* Trash reports Marker Data */ 
+var trashMarkers = [
+    [{
+        position: {lat:21.300776, lng:-158.050877},
+    }, {
+        img: 'images/nice.jpg'
+    }
+    ], 
+    [{
+        position: {lat:21.301776, lng:-158.050877}
+    },
+    {
+        img: 'images/nice.jpg'
+    }
+    ]
+]; //array[{markerinfo},{elementsForGUI}]
--- magic.js
+++ magic.js
@@ -1,8 +1,69 @@
-console.log('oof');
-var map;
-function initMap() {
-  map = new google.maps.Map(document.querySelector('#map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+var map; //map for the API 
+var allLocation = []; //array for all the locations on where to measure trash
+var allMarkers = []; //array for all the markers
+
+const statsGUI = document.querySelector('#statistics');
+const report = document.querySelector('report');
+
+function initMap() { //initation
+  map = new google.maps.Map(document.querySelector('#map'), { // where the map is located in HMTL
+    center: {lat:21.300776, lng:-158.051877}, //where it will be defaulted too
+    zoom: 15 //zoom of how close it is
   });
-}
+
+  map.addListener('click', () => { //resets the visibility of the markers
+    return allMarkers.forEach((cur) => {
+      return cur.setVisible(true)
+    });
+  });
+
+  trashMarkers.forEach(cur => { //gets trash markers from data
+    let marker = new google.maps.Marker(cur[0]);
+    marker.addListener('click',function() {
+      console.log('nice');
+    });
+    marker.setMap(map);
+    allMarkers.push(marker);
+  });
+
+  // for(var coords in beachLocations) { //makes polygon from coordinates
+  //   let location = new google.maps.Polygon({
+  //     paths: beachLocations[coords],
+  //     strokeColor: SeverityColor(this),
+  //     strokeOpacity: 0.8,
+  //     strokeWeight: 2,
+  //     fillColor: SeverityColor(this),
+  //     fillOpacity: 0.35
+  //   });
+  //   location.setMap(map);
+  //   allLocation.push(location);
+  // };
+
+  // function SeverityColor(polygon) {
+  //   let markerCount = google.maps.geometry.poly.containsLocation(cur.position, polygon) ? true: false;
+  //   console.log(markerCount);
+  // };
+
+  /* sets the test locations */
+  var testLocation = new google.maps.Polygon({ /* testing purposes */
+    paths: beachLocations.whiteplains,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  testLocation.addListener('click',function() { //if area clicked; marker for area appear
+    allMarkers.map((cur) => { 
+      return google.maps.geometry.poly.containsLocation(cur.position, testLocation) ? cur.setVisible(true) : cur.setVisible(false) //whether marker visible or not
+    }); 
+  }); //problem: need more dynamic marker to location ratio
+  //testLocation.addListener('click',function(){console.log('oof')});
+  /* test location end */
+  
+  /* sets polygons and permanent markers to set location */
+  testLocation.setMap(map);
+};
+
+console.log(allMarkers);
